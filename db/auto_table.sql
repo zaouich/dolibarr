121743c7483b2cb0ba39d7639b9ccceb352924a8
zaouich <121829336+zaouich@users.noreply.github.com>
2023-04-01 16:00:32 +0000

    Create auto_table.sql

---
+++ db/auto_table.sql
@@ -0,0 +1,23 @@
+use dolibarr
+drop table llx_auto_send
+CREATE TABLE llx_auto_send (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  sms_after_create_invoice TINYINT(1) NOT NULL DEFAULT 0,
+  email_after_create_invoice TINYINT(1) NOT NULL DEFAULT 0,
+  sms_after_delete_invoice TINYINT(1) NOT NULL DEFAULT 0,
+  email_after_delete_invoice TINYINT(1) NOT NULL DEFAULT 0,
+  sms_afteradd_payment TINYINT(1) NOT NULL DEFAULT 0,
+  email_afteradd_payment TINYINT(1) NOT NULL DEFAULT 0,
+  sms_after_delete_payment TINYINT(1) NOT NULL DEFAULT 0,
+	email_after_delete_payment TINYINT(1) NOT NULL DEFAULT 0,
+  sms_after_cancel_payment TINYINT(1) NOT NULL DEFAULT 0,
+  email_after_cancel_payment TINYINT(1) NOT NULL DEFAULT 0,
+  sms_after_reopen_invoice TINYINT(1) NOT NULL DEFAULT 0,
+  email_after_reopen_invoice TINYINT(1) NOT NULL DEFAULT 0,
+  sms_after_classify_as_paid TINYINT(1) NOT NULL DEFAULT 0,
+  email_after_classify_as_paid TINYINT(1) NOT NULL DEFAULT 0,
+  sms_after_classify_as_paid_partially TINYINT(1) NOT NULL DEFAULT 0,
+   email_after_classify_as_paid_partially TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+);
+
